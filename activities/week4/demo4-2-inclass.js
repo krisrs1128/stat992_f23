61d2357db2a42dda136ce31ed5c18294be0b715d
Kris Sankaran <sankaran.kris@gmail.com>
2023-10-09 16:51:00 -0500

    in-class solution

---
+++ activities/week4/demo4-2-inclass.js
@@ -0,0 +1,33 @@
+
+// fill in solutions here
+
+// part c
+d3.select("#part_c")
+  .attrs({
+    cx: 100,
+    cy: 100
+  })
+
+  // part d
+  //d3.selectAll(".part_d")
+  //  .attrs({
+  //    fill: "red"
+  //  })
+
+// part e
+//d3.select("#group_part_e")
+ // .selectAll(".part_e")
+  //.attrs({
+   // fill: "red"
+  //})
+
+
+
+
+
+
+
+
+
+
+
