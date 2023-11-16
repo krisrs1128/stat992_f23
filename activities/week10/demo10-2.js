95292a05f32788e9f679f23bf54ee5f16ed0b6b7
Kris Sankaran <sankaran.kris@gmail.com>
2023-11-16 10:54:07 -0600

    slight revision to approach

--- activities/week10/demo10-2.js
+++ activities/week10/demo10-2.js
@@ -3,18 +3,10 @@ function setup_simulation(data) {
   let [nodes, links] = data
   let simulation = d3.forceSimulation(nodes)
     .force("link", d3.forceLink(links).id(d => d.id)) // attracts nodes
-    .force("charge", d3.forceManyBody().strength(-8)) // repels nodes
+    .force("charge", d3.forceManyBody().strength(-10)) // repels nodes
     .force("center", d3.forceCenter(300, 300)) // center of the canvas
 
   return [simulation, nodes, links]
-}
-
-function visualize(data) {
-  let [simulation, nodes, links] = setup_simulation(data)
-  n_array = neighborhoods(nodes, links)
-
-  initialize_graph(nodes, links)
-  simulation.on("tick", _ => ticked(nodes))
 }
 
 function initialize_graph(nodes, links, scales) {
@@ -42,12 +34,12 @@ function initialize_graph(nodes, links, scales) {
     })
 }
 
-function ticked(nodes) {
+function ticked(nodes, neighbor_arrays) {
   d3.select("#nodes")
     .selectAll("circle")
     .attrs({
       cx: d => d.x,
-      cy: d => d.y,
+      cy: d => d.y
     })
 
   d3.select("#links")
@@ -58,34 +50,45 @@ function ticked(nodes) {
       y1: d => d.source.y,
       y2: d => d.target.y
     })
-
   voronoi = d3.Delaunay.from(nodes.map(d => [d.x, d.y]))
-  d3.select("#hover").on("mousemove", ev => update_neighbors(ev, nodes, voronoi))
+  d3.select("svg").on("mousemove", ev => update_neighbors(ev, voronoi, nodes, neighbor_arrays))
 }
 
-function update_neighbors(ev, nodes, voronoi) {
+function update_neighbors(ev, voronoi, nodes, neighbor_arrays) {
   let pos = d3.pointer(ev)
-  ix = voronoi.find(pos[0], pos[1])
+  let ix = voronoi.find(pos[0], pos[1])
 
-  d3.select("#nodes")
-    .selectAll("circle")
-    .transition().duration(1000)
-    .attrs({ r: d => d.id == nodes[ix].id ? 5 : 3 })
+  d3.selectAll("circle")
+    .attrs({ r: d => neighbor_arrays[ix].indexOf(d.id) == -1? 3: 6 })
 
-  selection = d3.select("#labels")
+  cur_nodes = neighbor_arrays[ix].map(i => nodes[i])
+  let text_elem = d3.select("#labels")
     .selectAll("text")
-    .data([nodes[ix].label], d => d)
+    .data(cur_nodes, d => d.id)
 
-  selection.attrs({
-    transform: `translate(${nodes[ix].x},${nodes[ix].y})`
-  })
-  selection.enter()
+  text_elem.enter()
     .append("text")
-    .text(d => d)
-  selection.exit().remove()
+    .attrs({
+      "font-size": d => d.id == nodes[ix].id ? 12 : 8,
+      transform: d => `translate(${d.x},${d.y})`
+    })
+
+  text_elem.transition()
+    .attrs({ "font-size": d => d.id == nodes[ix].id ? 12 : 8})
+    .text(d => d.label)
+  
+  text_elem.exit().remove()
 }
 
-let transition_length = 1000
+function visualize(data) {
+  let [simulation, nodes, links] = setup_simulation(data)
+  neighbor_arrays = neighborhoods(nodes, links)
+
+  initialize_graph(nodes, links)
+  simulation.on("tick", _ => ticked(nodes, neighbor_arrays))
+}
+
+let transition_length = 500
 Promise.all([
   d3.csv("political-books-nodes.csv", d3.autoType),
   d3.csv("political-books-edges.csv", d3.autoType)
