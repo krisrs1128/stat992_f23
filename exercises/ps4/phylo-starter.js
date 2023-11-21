ec84d87fa1ea8cefb7fb3c092dcf3650d9888950
Kris Sankaran <sankaran.kris@gmail.com>
2023-11-20 22:32:45 -0600

    starter analogous to 10-1

--- exercises/ps4/phylo-starter.js
+++ exercises/ps4/phylo-starter.js
@@ -1,9 +1,12 @@
 
 function make_tree(edges) {
   edges.push({to: 1, from: null});
-  stratifier = d3.stratify(edges)
+  stratifier = d3.stratify()
     .id(d => d.to)
     .parentId(d => d.from)
+  
+  let root = stratifier(edges)
+  // apply d3.tree()-based function to root
 }
 
 function visualize(data) {
